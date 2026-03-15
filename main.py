5e579bf65e6ec00320d20c1b3358f46ca60dbbfe
YaroslavItStep <viktorholyak1@gmail.com>
2026-03-15 10:56:00 +0200

    New

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("1213")
+class Human:
+    def __init__(self, name = "Human"):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+    def add_passengers(self, *args):
+        for passengers in args:
+            self.passengers.append(passengers)
+    def print_passengers_names(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers:")
+            for passengers in self.passengers:
+                print(passengers.name)
+        else:
+            print("Aint passengers")
+
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Mercedes")
+
+car.add_passengers(nick, kate)
+car.print_passengers_names()
